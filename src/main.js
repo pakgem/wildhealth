4f4fe8f240f20756bda09c9e65615b568e7aadda
Gemin Pak <geminpak@gmail.com>
2022-09-10 22:04:42 -0700

    Add in banner geolocation display logic

--- src/main.js
+++ src/main.js
@@ -1,3 +1,25 @@
 import './styles/style.css'
+import $ from 'jquery'
 
-console.log('Welcome to Vite + JS + Webflow <3')
+displayBanner()
+
+// Display Banner
+function displayBanner() {
+  let userLoc = JSON.parse(httpGet('https://get.geojs.io/v1/ip/geo.json'))
+  let userState = userLoc['region']
+  // if geo banner exists
+  if ($('.section-banner-geo').length) {
+    let bannerLoc = document.getElementById('bannerLoc').innerText
+    if (userState == bannerLoc) {
+      console.log('State match')
+      $('.section-banner-geo').show()
+    }
+  }
+}
+
+function httpGet(theUrl) {
+  var xmlHttp = new XMLHttpRequest()
+  xmlHttp.open('GET', theUrl, false) // false for synchronous request
+  xmlHttp.send(null)
+  return xmlHttp.responseText
+}
