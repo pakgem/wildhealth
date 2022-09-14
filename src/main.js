75d6c85e84ceb8d1a0551d550fb17d522695067b
Gemin Pak <geminpak@gmail.com>
2022-09-14 12:49:06 -0700

    Adding visibility code to banner

--- src/main.js
+++ src/main.js
@@ -7,12 +7,17 @@ displayBanner()
 function displayBanner() {
   let userLoc = JSON.parse(httpGet('https://get.geojs.io/v1/ip/geo.json'))
   let userState = userLoc['region']
+  console.log(userState)
   // if geo banner exists
   if ($('.section-banner-geo').length) {
     let bannerLoc = document.getElementById('bannerLoc').innerText
     if (userState == bannerLoc) {
       console.log('State match')
-      $('.section-banner-geo').show()
+      //$('.section-banner-geo').show()
+      $('.section-banner-geo').css('height', '54px')
+      $('.section-banner-geo').css('opacity', '100%')
+
+      $('.section-droppop').hide()
     }
   }
 }
