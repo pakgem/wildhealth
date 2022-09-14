16c70c851b00d061cb981699f9eea0d2c895fa6f
Gemin Pak <geminpak@gmail.com>
2022-09-14 13:59:20 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,16 +7,15 @@ displayBanner()
 function displayBanner() {
   let userLoc = JSON.parse(httpGet('https://get.geojs.io/v1/ip/geo.json'))
   let userState = userLoc['region']
-  console.log(userState)
+  //console.log(userState)
   // if geo banner exists
   if ($('.section-banner-geo').length) {
     let bannerLoc = document.getElementById('bannerLoc').innerText
     if (userState == bannerLoc) {
-      console.log('State match')
+      //console.log('State match')
       //$('.section-banner-geo').show()
-      $('.section-banner-geo').css('min-height', '54px')
+      $('.section-banner-geo').css('height', 'auto')
       $('.section-banner-geo').css('opacity', '100%')
-
       $('.section-droppop').remove()
     }
   }
