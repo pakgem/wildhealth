d2f4f836e15d9f1a81942d90616130956dc522b2
Gemin Pak <geminpak@gmail.com>
2022-09-16 14:42:25 -0700

    Fix userstate check

--- src/main.js
+++ src/main.js
@@ -10,8 +10,10 @@ function displayBanner() {
   // if geo banner exists
   if ($('.section-banner-geo').length) {
     let bannerLoc = document.getElementById('bannerLoc').innerText.toLowerCase()
+    console.log(userState, bannerLoc)
+
     // if user's state matches the state set in banner
-    if (bannerLoc.indexOf(userState != -1)) {
+    if (bannerLoc.indexOf(userState) != -1) {
       $('.section-banner-geo').css('height', 'auto')
       $('.section-banner-geo').css('opacity', '100%')
       $('.section-droppop, .section-banner').remove()
