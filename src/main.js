82cb563d5ab6494d18a9ef8638d4779cead14e34
Gemin Pak <geminpak@gmail.com>
2022-09-16 13:17:05 -0700

    Update to allow multiple states set in banner

--- src/main.js
+++ src/main.js
@@ -6,14 +6,12 @@ displayBanner()
 // Display Banner
 function displayBanner() {
   let userLoc = JSON.parse(httpGet('https://get.geojs.io/v1/ip/geo.json'))
-  let userState = userLoc['region']
-  //console.log(userState)
+  let userState = userLoc['region'].toLowerCase()
   // if geo banner exists
   if ($('.section-banner-geo').length) {
-    let bannerLoc = document.getElementById('bannerLoc').innerText
-    if (userState == bannerLoc) {
-      //console.log('State match')
-      //$('.section-banner-geo').show()
+    let bannerLoc = document.getElementById('bannerLoc').innerText.toLowerCase()
+    // if user's state matches the state set in banner
+    if (bannerLoc.indexOf(userState != -1)) {
       $('.section-banner-geo').css('height', 'auto')
       $('.section-banner-geo').css('opacity', '100%')
       $('.section-droppop, .section-banner').remove()
