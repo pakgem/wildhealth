237af7808bfe8e87b96c61f380ccd21eaaea8caf
Gemin Pak <geminpak@gmail.com>
2022-09-21 06:15:12 -0700

    Update banner removal logic

--- src/main.js
+++ src/main.js
@@ -14,9 +14,10 @@ function displayBanner() {
 
     // if user's state matches the state set in banner
     if (bannerLoc.indexOf(userState) != -1) {
-      $('.section-banner-geo').css('height', 'auto')
+      $('.section-banner-geo').css('height', '3.5rem')
       $('.section-banner-geo').css('opacity', '100%')
-      $('.section-droppop, .section-banner').remove()
+      $('.section-droppop, .section-banner').css('opacity', '0%')
+      $('.section-droppop, .section-banner').css('height', '0rem')
     }
   }
 }
