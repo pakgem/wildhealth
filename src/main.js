8dc169df67fd0e236fcb83cf0f4886013727265d
Gemin Pak <geminpak@gmail.com>
2022-12-14 18:13:21 -0800

    Add functionality to take in a list of states for banner

--- src/main.js
+++ src/main.js
@@ -6,18 +6,17 @@ displayBanner()
 // Display Banner
 function displayBanner() {
   let userLoc = JSON.parse(httpGet('https://get.geojs.io/v1/ip/geo.json'))
-  let userState = userLoc['region'].toLowerCase()
+  let userState = getStateAcronym(userLoc['region'])
   // if geo banner exists
   if ($('.section-banner-geo').length) {
-    let bannerLoc = document.getElementById('bannerLoc').innerText.toLowerCase()
-    console.log(userState, bannerLoc)
+    let bannerLocStr = document.getElementById('bannerLoc').innerText
+    let locItems = splitString(bannerLocStr)
 
-    // if user's state matches the state set in banner
-    if (bannerLoc.indexOf(userState) != -1) {
+    // if user's state matches the state list set in banner settings
+    if (locItems.includes(userState)) {
       $('.section-banner-geo').css('height', '3.5rem')
       $('.section-banner-geo').css('opacity', '100%')
-      $('.section-droppop, .section-banner').css('opacity', '0%')
-      $('.section-droppop, .section-banner').css('height', '0rem')
+      $('.section-droppop, .section-banner').remove()
     }
   }
 }
@@ -28,3 +27,76 @@ function httpGet(theUrl) {
   xmlHttp.send(null)
   return xmlHttp.responseText
 }
+
+function getStateAcronym(stateName) {
+  // Create an object that maps state names to their acronyms
+  const stateAcronyms = {
+    Alabama: 'AL',
+    Alaska: 'AK',
+    Arizona: 'AZ',
+    Arkansas: 'AR',
+    California: 'CA',
+    Colorado: 'CO',
+    Connecticut: 'CT',
+    Delaware: 'DE',
+    Florida: 'FL',
+    Georgia: 'GA',
+    Hawaii: 'HI',
+    Idaho: 'ID',
+    Illinois: 'IL',
+    Indiana: 'IN',
+    Iowa: 'IA',
+    Kansas: 'KS',
+    Kentucky: 'KY',
+    Louisiana: 'LA',
+    Maine: 'ME',
+    Maryland: 'MD',
+    Massachusetts: 'MA',
+    Michigan: 'MI',
+    Minnesota: 'MN',
+    Mississippi: 'MS',
+    Missouri: 'MO',
+    Montana: 'MT',
+    Nebraska: 'NE',
+    Nevada: 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    Ohio: 'OH',
+    Oklahoma: 'OK',
+    Oregon: 'OR',
+    Pennsylvania: 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    Tennessee: 'TN',
+    Texas: 'TX',
+    Utah: 'UT',
+    Vermont: 'VT',
+    Virginia: 'VA',
+    Washington: 'WA',
+    'West Virginia': 'WV',
+    Wisconsin: 'WI',
+    Wyoming: 'WY',
+  }
+
+  // Return the acronym for the given state, if it exists
+  return stateAcronyms[stateName] || null
+}
+
+function splitString(str) {
+  // Split the string into an array of items separated by a comma
+  const items = str.split(', ')
+  // Return the array of items
+  return items
+}
+
+$(document).ready(function () {
+  if (document.referrer.indexOf(window.location.hostname) != -1) {
+    var referrer = document.referrer
+    console.log(referrer)
+  }
+})
